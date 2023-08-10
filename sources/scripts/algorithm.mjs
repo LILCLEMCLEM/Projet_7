f39e5e90b39120b9d3a64ff5fa7adf10f7b8d6bc
LILCLEMCLEM <clement59lesne@gmail.com>
2023-08-10 12:45:59 +0200

    fix timer

--- sources/scripts/algorithm.mjs
+++ sources/scripts/algorithm.mjs
@@ -86,7 +86,12 @@ class algorithm {
         this.setRecettesValue()
         this.F.load_filters(recipes)
         updateFilters(searchbar_list)
-        t.stop();
+        try {
+            t.stop();
+        }
+        catch  {
+            console.log("")
+        }
         
     }
 
